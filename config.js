f12fd04898b489300e75c726543a0e1552c30903
Alvyn Vengeance <94638000+alvyn-vengeance@users.noreply.github.com>
2022-06-26 12:51:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,148 @@
+//════════════════════════════//
+//If you want to recode, reupload
+//or copy the codes/script,
+//pls give credit
+//no credit? i will take action immediately
+//© 2022 Xeon Bot Inc. Cheems Bot MD
+//Thank you to Lord Buddha, Family and Myself
+//════════════════════════════//
+//recode kar ke youtube pe upload kar rhe ya
+//codes copy kar ke apne script me dal rhe
+//hai to, description me xeon ka yt channel
+// ka link paste kr dena as a cradit or github 
+//repo me bhi tag kardena baki jo
+//bhi karna hai apki marzi, thank you!🦄
+//════════════════════════════//
+//If you recode and uploading on your channel
+//or copy pasting the codes in ur script, 
+//i give permission to do as long as you
+//put Xeons youtube channel link in the video
+//description and tag me on githuh repo, 
+//thank you🦄
+//════════════════════════════//
+
+const fs = require("fs")
+const chalk = require("chalk")
+
+//aumto functioner
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['62859193578139'] //ur owner number
+global.ownername = "ｖｙｎｕｔｚｘｘ２２"
+global.ytname = "hhttps://linktr.ee/vynutzxx22" //ur yt chanel name
+global.socialm = "https://instagram.com/vynutzxx22" //ur github or insta name
+global.location = "Medan, Indonesia" //ur location
+
+//bot bombdy
+global.botname = "ALVYN-BOTZ" //ur bot name
+global.websitex = "https://chat.whatsapp.com/C3mbk4fnGHW3LbEEtFgj8T" //ur website
+global.vidmenu = { url: 'https://telegra.ph/file/fe3a0e1eb5ee41bb82df1.mp4' } //gif and video menu
+global.packname = "@ ALVYN-BOTZ"
+global.author = "ｖｙｎｕｔｚｘｘ２２"
+global.themeemoji = "🐶"
+global.reactmoji = "😘"
+global.ownertag = ['62859193578139'] //ur owner tag
+global.ownernummenu = ['62859193578139'] //ur owner number in menu and all
+global.watermark = "『  𝐀𝐋𝐕𝐘𝐍-𝐁𝐎𝐓𝐙  』" //ur watermark
+global.botscript = "https://github.com/alvyn-vengeance/"
+global.linkz1 = "https://chat.whatsapp.com/C3mbk4fnGHW3LbEEtFgj8T"
+global.linkz2 = "https://chat.whatsapp.com/C3mbk4fnGHW3LbEEtFgj8T"
+
+//Bot theme media
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//database
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.lolhuman = "KaysaS"
+global.sessionName = "session"
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: '✅ _*Done Wibu*_',
+    admin: '🙅 _*Fitur Khusus Admin Group!*_',
+    botAdmin: '🙏 _*Bot Harus Menjadi Admin Terlebih Dahulu!*_',
+    owner: '🙅 _*Fitur Khusus Owner Bot*_',
+    group: '_*Maaf 🙏, Untuk saat ini bot hanya bisa digunakan didalam Grup 👥, Silahkan Join Grup Bot : https://chat.whatsapp.com/C3mbk4fnGHW3LbEEtFgj8T*_',
+    private: '👤 _*Fitur Digunakan Hanya Untuk Private Chat!*_',
+    bot: '❎ _*Fitur Khusus Pengguna Nomor Bot*_',
+    wait: '⏳ _*Loading... Sabar Wibu*_',
+    linkm: '⚠️ _*Masukkan link*_',
+    error: '⚠️ _*Error!*_',
+    ban: '⚠️ _*Kamu sudah dibanned oleh bot, jika ingin dibuka bannednya chat owner: wa.me/62859193578139*_',
+    nsfw: '⚠️ _*Fitur NSFW tidak diaktifkan, chat admin grup supaya diaktifkan*_',
+    banChat: '⚠️ *_Bot sudah dimute/banned dari grup ini, chat pemilik bot untuk dibuka bannednya:*_\n*wa.me/62859193578139*'
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 10,
+    monayawal: 500
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 10
+}
+// api website by xenz
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// key
+global.APIKeys = {
+	'https://zenzapis.xyz': '743648144832',
+}
+//menu logo maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
